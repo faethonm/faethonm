ce0d7c4c1cb726d810da0602b487da56c31c9dfa
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-04-26 10:23:50 -0700

    fix broken links and text

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,12 @@ end
 
 User.create(email: 'test@example.com', password: 'password')
 Project.create(title: 'Foggy',
-               url: 'https://hihiri.herokuapp.com/',
+               url: 'http://www.askfoggy.com',
                description: 'We take of the little things for you so you can enjoy your big moments',
                image: 'foggy.png')
 
 Project.create(title: 'Hihiri',
-               url: 'https://hihiri.herokuapp.com/',
+               url: 'https://hihiri.herokuapp.com',
                description: 'Achieve great things together',
                image: 'hihiri.png')
 
